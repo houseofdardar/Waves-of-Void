64fe69159004f255db2e41429b3dec1d66020e9a
houseofdardar <houseofdardar@gmail.com>
2020-02-28 13:29:34 -0800

    Initial commit

---
+++ Waves of Void - Copy/Assets/Score.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+}
