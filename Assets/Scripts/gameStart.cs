637d316784e5237c71e55e0178a5a8acff2da0af
houseofdardar <houseofdardar@gmail.com>
2020-03-10 12:59:36 -0700

    made player a prefab
    so ppl can use player in new sean

--- Assets/Scripts/gameStart.cs
+++ Assets/Scripts/gameStart.cs
@@ -8,7 +8,8 @@ public class gameStart : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       // myObject.GetComponent<MyScript>().MyFunction();
+
     }
 
     // Update is called once per frame
